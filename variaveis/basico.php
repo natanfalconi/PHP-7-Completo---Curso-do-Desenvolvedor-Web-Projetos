6d019cbceed169155dc9c0f7ed4660b7ce3658c7
natanfalconi <nfalconi65@gmail.com>
2024-03-19 13:59:25 -0300

    basico sobre variáveis

--- variaveis/basico.php
+++ variaveis/basico.php
@@ -20,4 +20,36 @@ echo '<br>';
 
 unset($somaDosNumeros); // essa variavel já não está setada
 // echo isset ($somaDosNumeros); 
-var_dump($somaDosNumeros); // resultado -- null
+// var_dump($somaDosNumeros); // resultado -- null
+
+$variavel = 'Antes era 10 (int), Agora sou uma string';
+echo '<br>' . $variavel;
+
+// nomes de variáveis
+// precisa do dolar em php
+$var = "valida";
+$var2 = "valida";
+
+// pode ser tudo maiuscula com numeros
+$VAR3 = 'valida';
+$_var_4 = 'valida';
+
+// IDEAL É SER CONSISTENTE NA CRIAÇÃO DAS VARIÁVEIS - SEGUIR UMA MESMA ESTRUTURA - UMA PADRONIZAÇÃO
+
+
+// é valido, mas não é recomendado esse tipo de variável, com acentuação0
+// EVITAR
+$vár5 = 'valida';
+
+// NÃO PERMITGE NESSES CASOS
+// $6var = 'INVÁLIDA';
+// $%VAR7 = 'INVÁLIDA';
+// $VAR8% = 'INVÁLIDA';
+
+
+
+
+// VARIÁVEIS PRÉ DEFINIDAS
+
+echo '<br>';
+var_dump($_SERVER["HTTP_HOST"]);
