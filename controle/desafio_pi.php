5c1fbef7298e87582760c7bf2d1ae60d81599052
natanfalconi <nfalconi65@gmail.com>
2024-03-25 19:09:10 -0300

    op. lógicos

--- controle/desafio_pi.php
+++ controle/desafio_pi.php
@@ -2,11 +2,27 @@
 
 <?php
 
+//desafio pi
 echo pi();
 $pi = 3.14;
 
-if (pi() <=> $pi) {
+if (pi() === $pi) {
     echo "<br>Iguais";
 } else {
     echo "<br> Diferentes";
+}
+
+//resposta
+$piErrado = 2.8;
+echo "<br>" . ($pi - pi());
+echo "<br>" . ($pi - $piErrado);
+
+if ($pi - pi() <= 0.01) {
+    echo "<br> Praticamente Iguais";
+}
+
+if ($pi - $piErrado <= 0.01) {
+    echo "<br> Praticamente Iguais";
+} else {
+    echo "<br> Valor err ado";
 }
